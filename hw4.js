5d0ad836d41f1a74b20a3670727f9f9d574dc027
michaelcodes <michaelletsgo@Michaels-MacBook-Pro.local>
2021-03-18 22:01:06 -0400

    objectception complete

---
+++ hw4.js
@@ -0,0 +1,22 @@
+//Change the value of linbo to null
+console.log("starting hw4");    
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: { dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
+    }
+ }
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null
+console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
+
