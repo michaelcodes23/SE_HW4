e5faae791f06095a1d5532d584bad6fbf4b39937
michaelcodes23 <cuencamich@gmail.com>
2021-03-20 10:04:19 -0400

    Committing my DOM

---
+++ Domtree/app.js
@@ -0,0 +1,6 @@
+console.log("hi")
+console.log("Welcome to my Software Engineering course!")
+let secondHeading = document.querySelector('h2');
+console.log(h2);
+let all = document.querySelectorAll('body');
+console.log(all)
